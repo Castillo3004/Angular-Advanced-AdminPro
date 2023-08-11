cba1ea9672ae39749466276b23a45b112eb0b77b
Castillo3004 <cacastillo34@utpl.edu.ec>
2023-08-11 13:21:47 -0500

    fin seccion 3 | Diseño

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRountingModule } from './app-rounting.module';
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NoPageFoundComponent } from './pages/no-page-found/no-page-found.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BreadCrumbsComponent } from './shared/bread-crumbs/bread-crumbs.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { PagesComponent } from './pages/pages.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NoPageFoundComponent,
+    DashboardComponent,
+    BreadCrumbsComponent,
+    SidebarComponent,
+    HeaderComponent,
+    ProgressComponent,
+    Grafica1Component,
+    PagesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRountingModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
